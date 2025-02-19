6682a6452b5bcfe043403bd9ac49d5342df6db78
ck.lee <ck.lee@mimos.my>
2025-02-19 09:10:12 +0800

    Change to working endpoints

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import axios from 'axios';
 import {AuthContext, AuthProvider, IAuthContext, TAuthConfig, TRefreshTokenExpiredEvent} from "react-oauth2-code-pkce"
 
 const authConfig: TAuthConfig = {
-    clientId: 'lydtech-public-client',
-    authorizationEndpoint: 'http://localhost:8080/realms/lydtech/protocol/openid-connect/auth',
-    tokenEndpoint: 'http://localhost:8080/realms/lydtech/protocol/openid-connect/token',
-    logoutEndpoint: 'http://localhost:8080/realms/lydtech/protocol/openid-connect/logout',
-    redirectUri: 'http://localhost:3000',
+    clientId: 'public-client',
+    authorizationEndpoint: 'https://app.please-open.it/auth/realms/ee1afd72-71a9-49ad-a975-54ed46cc56a3/protocol/openid-connect/auth',
+    tokenEndpoint: 'https://app.please-open.it/auth/realms/ee1afd72-71a9-49ad-a975-54ed46cc56a3/protocol/openid-connect/token',
+    logoutEndpoint: 'https://app.please-open.it/auth/realms/ee1afd72-71a9-49ad-a975-54ed46cc56a3/protocol/openid-connect/logout',
+    // redirectUri: 'https://prawn-humble-mackerel.ngrok-free.app',
+    // redirectUri: 'http://localhost:3000',
+    redirectUri: 'http://10.45.16.118:3000', // Only work in Desktop Chrome after follow https://stackoverflow.com/a/78397011/418439
     scope: 'openid',
-    onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
+    // TODO: Temporary comment to see if the refresh token is working
+    // onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
 }
 
 const callEndpoint = async (url: string, token: string) => {
@@ -42,24 +45,25 @@ const callEndpoint = async (url: string, token: string) => {
     return `called at ${datetime} - ${message}`;
 }
 
-const UserInfo = (): JSX.Element => {
+const resourceServerDomain = 'http://10.45.16.118:8081';
+const   UserInfo = (): JSX.Element => {
     const {token, tokenData, logOut} = useContext<IAuthContext>(AuthContext)
     const [publicResponse, setPublicResponse] = useState("Not called yet");
     const [adminResponse, setAdminResponse] = useState("Not called yet");
     const [userResponse, setUserResponse] = useState("Not called yet");
 
     const callPublic = async () => {
-        const message = await callEndpoint('http://localhost:8081/api/v1/public', token);
+        const message = await callEndpoint(resourceServerDomain + '/api/v1/public', token);
         setPublicResponse(message);
     }
 
     const callAdmin = async () => {
-        const message = await callEndpoint('http://localhost:8081/api/v1/admin', token);
+        const message = await callEndpoint(resourceServerDomain + '/api/v1/admin', token);
         setAdminResponse(message);
     }
 
     const callUser = async () => {
-        const message = await callEndpoint('http://localhost:8081/api/v1/user', token);
+        const message = await callEndpoint(resourceServerDomain + '/api/v1/user', token);
         setUserResponse(message);
     }
 
